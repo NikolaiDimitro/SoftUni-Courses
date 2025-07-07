33711e7402cf9398a39d76f2412e6527e3a1d950
Nikolay Dimitrov <156689620+NikolaiDimitro@users.noreply.github.com>
2025-07-07 17:25:11 +0300

    Delete Programming Basic With Javascript/13. More Exercises/01. First Steps in Coding directory

--- Programming Basic With Javascript/13. More Exercises/01. First Steps in Coding/03. Celsius to Fahrenheit/03. Celsius to Fahrenheit.js
+++
@@ -1,9 +0,0 @@
-function celsiusToFahrenheit(c) {
-
-    console.log(((c * 1.8) + 32).toFixed(2));
-
-}
-celsiusToFahrenheit(25);
-celsiusToFahrenheit(0);
-celsiusToFahrenheit(-5.5);
-celsiusToFahrenheit(32.3);
