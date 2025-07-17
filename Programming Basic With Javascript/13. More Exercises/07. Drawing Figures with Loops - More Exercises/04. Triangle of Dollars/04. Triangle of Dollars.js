f350753dae859f1b245b576f2862791b019262b9
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2025-07-17 21:26:09 +0300

    Triangle of Dollars - Drawing Figures With Loops - More Exercises

---
+++ Programming Basic With Javascript/13. More Exercises/07. Drawing Figures with Loops - More Exercises/04. Triangle of Dollars/04. Triangle of Dollars.js
@@ -0,0 +1,15 @@
+function triangleOfDollars(n) {
+
+    let print = '';
+
+    for (let rows = 1; rows <= n; rows++) {
+
+            print += '$ ';
+            console.log(print);
+
+    }
+}
+triangleOfDollars(2);
+triangleOfDollars(3);
+triangleOfDollars(4);
+triangleOfDollars(5);
