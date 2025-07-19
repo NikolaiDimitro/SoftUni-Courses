a1fa099e32cc7d42408fd52e2dfb328f46c1fb24
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2025-07-19 17:09:31 +0300

    Each Lunch - JS Basic 3

---
+++ Programming Basic With Javascript/14. JS Basic - Exam/03. Programming Basics Online Example Exam 3/01. Easter Lunch/01. Easter Lunch.js
@@ -0,0 +1,8 @@
+function eachLunch(countCozunaci, counteEggShells, kiloCookies) {
+
+    console.log((countCozunaci * 3.2 + counteEggShells * 4.35 + kiloCookies * 5.4 + (counteEggShells * 12 * 0.15)).toFixed(2));
+
+}
+eachLunch(3, 2, 3);
+eachLunch(4, 4, 3);
+eachLunch(2, 3, 2);
