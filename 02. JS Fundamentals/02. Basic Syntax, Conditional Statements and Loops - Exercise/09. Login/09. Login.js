1f091ebe74011f2cd37e839fa311b399167cc828
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2025-07-22 23:55:46 +0300

    Login - Basic Syntax, Conditional Statements and Loops - Exercise

---
+++ 02. JS Fundamentals/02. Basic Syntax, Conditional Statements and Loops - Exercise/09. Login/09. Login.js
@@ -0,0 +1,36 @@
+function login(arr) {
+
+    let username = arr[0];
+    let password = username.split('').reverse().join('');
+    let count = 0;
+
+    // console.log(password);
+
+    for (let i = 1; i < arr.length; i++) {
+
+        let current = arr[i];
+
+        if (current === password) {
+
+            console.log(`User ${username} logged in.`);
+
+        } else {
+
+            count++;
+
+            if (count === 4) {
+
+                console.log(`User ${username} blocked!`);
+                return;
+
+
+            }
+
+            console.log(`Incorrect password. Try again.`);
+
+        }
+    }
+}
+login(['Acer', 'login', 'go', 'let me in', 'recA']);
+login(['momo', 'omom']);
+login(['sunny', 'rainy', 'cloudy', 'sunny', 'not sunny']);
