481158b66716cfa7fc9e08f9952a71fde8bcc029
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2025-07-23 01:12:10 +0300

    А new version of the same solution has been added

--- 02. JS Fundamentals/03. Basic Syntax, Conditional Statements and Loops - More Exercise/01. Sort Numbers/01. Sort Numbers.js
+++ 02. JS Fundamentals/03. Basic Syntax, Conditional Statements and Loops - More Exercise/01. Sort Numbers/01. Sort Numbers.js
@@ -1,17 +1,73 @@
-function sortNumbers(x, y, z) {
+function sortNumbers(num1, num2, num3) {
 
-    let arr = Array(x, y, z);
-    let sortedArr = arr.sort((a, b) => b - a);
+    if (num1 > num2 && num1 > num3) {
 
-    let first = sortedArr[0];
-    let second = sortedArr[1];
-    let three = sortedArr[2];
+        console.log(num1);
 
-    console.log(first);
-    console.log(second);
-    console.log(three);
+        if (num2 > num3) {
 
+            console.log(num2);
+            console.log(num3);
+
+        } else {
+
+            console.log(num3);
+            console.log(num2);
+
+        }
+
+    } else if (num2 > num1 && num2 > num3) {
+
+        console.log(num2);
+
+        if (num1 > num3) {
+
+            console.log(num1);
+            console.log(num3);
+
+        } else {
+
+            console.log(num3);
+            console.log(num1);
+
+        }
+
+    } else if (num3 > num1 && num3 > num2) {
+
+        console.log(num3);
+
+        if (num1 > num2) {
+
+            console.log(num1);
+            console.log(num2);
+
+        } else {
+
+            console.log(num2);
+            console.log(num1);
+
+        }
+    }
 }
 sortNumbers(2, 1, 3);
 sortNumbers(-2, 1, 3);
 sortNumbers(0, 0, 2);
+
+
+// function sortNumbers(x, y, z) {
+
+//     let arr = Array(x, y, z);
+//     let sortedArr = arr.sort((a, b) => b - a);
+
+//     let first = sortedArr[0];
+//     let second = sortedArr[1];
+//     let three = sortedArr[2];
+
+//     console.log(first);
+//     console.log(second);
+//     console.log(three);
+
+// }
+// sortNumbers(2, 1, 3);
+// sortNumbers(-2, 1, 3);
+// sortNumbers(0, 0, 2);
