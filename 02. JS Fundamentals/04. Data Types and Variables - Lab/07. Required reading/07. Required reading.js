f37dff83ac1aa711c778265a794a95538e71351b
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2025-07-23 14:09:04 +0300

    Requred reading - Data Types and Variables - Lab

---
+++ 02. JS Fundamentals/04. Data Types and Variables - Lab/07. Required reading/07. Required reading.js
@@ -0,0 +1,7 @@
+function requiredReading(totalPages, pagesFor1Hour, countDays) {
+
+    console.log((totalPages / pagesFor1Hour) / countDays);
+
+}
+requiredReading(212, 20, 2);
+requiredReading(432, 15, 4);
