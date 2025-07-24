82692a424f2e4519b216001c8563f563de20de06
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2025-07-24 15:21:40 +0300

    Sum First and Last Array Elemnts - Array - Lab

---
+++ 02. JS Fundamentals/07. Arrays - Lab/01. Sum First and Last Array Elements/01. Sum First and Last Array Elements.js
@@ -0,0 +1,8 @@
+function sumFirstAndLastElements(arr) {
+
+    console.log(arr[0] + arr[arr.length - 1]);
+
+}
+sumFirstAndLastElements([20, 30, 40]);
+sumFirstAndLastElements([10, 17, 22, 33]);
+sumFirstAndLastElements([11, 58, 69]);
