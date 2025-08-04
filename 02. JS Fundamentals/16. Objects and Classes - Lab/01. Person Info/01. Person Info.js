5c952abecf5313378fd14d9f405b2bbc7e3e1b34
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2025-08-04 11:49:08 +0300

    Person Info

---
+++ 02. JS Fundamentals/16. Objects and Classes - Lab/01. Person Info/01. Person Info.js
@@ -0,0 +1,9 @@
+function personalInfo(firstName, lastName, age) {
+
+    let obj = { firstName: firstName, lastName: lastName, age: Number(age) };
+
+    return obj
+
+}
+personalInfo("Peter", "Pan", "20");
+personalInfo("George", "Smith", "18");
