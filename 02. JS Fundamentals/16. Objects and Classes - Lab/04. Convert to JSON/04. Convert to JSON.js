0d9f3577b73b73649d5ef043a44d37c982fdebbf
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2025-08-04 12:23:55 +0300

    Convert to JSON - Object and Classes Lab

---
+++ 02. JS Fundamentals/16. Objects and Classes - Lab/04. Convert to JSON/04. Convert to JSON.js
@@ -0,0 +1,9 @@
+function convertToJSON(firstName, lastName, hairColor) {
+
+    let obj = { name: firstName, lastName: lastName, hairColor: hairColor };
+    let json = JSON.stringify(obj);
+    console.log(json);
+
+}
+convertToJSON('George', 'Jones', 'Brown');
+convertToJSON('Peter', 'Smith', 'Blond');
