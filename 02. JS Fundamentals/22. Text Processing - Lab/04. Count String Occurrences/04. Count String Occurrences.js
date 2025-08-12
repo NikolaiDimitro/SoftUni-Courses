30c47d6a3c00eb65c38bd76c66c740647948b3d3
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2025-08-12 20:59:56 +0300

    Count String Occurrences

---
+++ 02. JS Fundamentals/22. Text Processing - Lab/04. Count String Occurrences/04. Count String Occurrences.js
@@ -0,0 +1,19 @@
+function countStringOccurrences(text, searchedWord) {
+
+    let words = text.split(' ');
+    let count = 0;
+
+    for (let word of words) {
+
+        if (word === searchedWord) {
+
+            count++;
+
+        }
+    }
+
+    console.log(count);
+
+}
+countStringOccurrences('This is a word and it also is a sentence', 'is');
+countStringOccurrences('softuni is great place for learning new programming languages', 'softuni');
