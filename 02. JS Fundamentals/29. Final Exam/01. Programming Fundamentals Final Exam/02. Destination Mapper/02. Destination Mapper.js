587d71502bbc89cabc8504b9d43da0d40595c645
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2025-08-21 23:48:22 +0300

    Destination Mapper - JS Fundamental Final Exam Retake

--- 02. JS Fundamentals/29. Final Exam/01. Programming Fundamentals Final Exam/02. Destination Mapper/02. Destination Mapper.js
+++ 02. JS Fundamentals/29. Final Exam/01. Programming Fundamentals Final Exam/02. Destination Mapper/02. Destination Mapper.js
@@ -1,9 +1,22 @@
 function destinationMapper(arr) {
 
+    let str = arr.shift();
+    let pattern = /([=\/])(?<city>[A-Z][A-Za-z]{2,})\1/g;
+    let exec = pattern.exec(str);
+    let newArr = [];
+    let sum = 0;
 
+    while (exec !== null) {
+
+        newArr.push(exec.groups.city);
+        sum += exec.groups.city.length;
+        exec = pattern.exec(str);
+
+    }
+
+    console.log(`Destinations: ${newArr.join(', ')}`);
+    console.log(`Travel Points: ${sum}`);
 
 }
 destinationMapper(['=Hawai=/Cyprus/=Invalid/invalid==i5valid=/I5valid/=i=']);
 destinationMapper(['ThisIs some InvalidInput']);
-// destinationMapper();
-// destinationMapper();
