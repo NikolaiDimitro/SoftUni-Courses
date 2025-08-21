9e9744d922bf5317c2f3f06e42a0ff0c3a0ee6e4
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2025-08-21 22:50:54 +0300

    Destination Mapper

---
+++ 02. JS Fundamentals/29. Final Exam/01. Programming Fundamentals Final Exam/02. Destination Mapper/02. Destination Mapper.js
@@ -0,0 +1,9 @@
+function destinationMapper(arr) {
+
+
+
+}
+destinationMapper(['=Hawai=/Cyprus/=Invalid/invalid==i5valid=/I5valid/=i=']);
+destinationMapper(['ThisIs some InvalidInput']);
+// destinationMapper();
+// destinationMapper();
