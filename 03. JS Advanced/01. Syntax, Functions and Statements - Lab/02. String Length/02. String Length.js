a0a59ad1c3209d711c22c6ff51aec46fbbfeb980
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2025-09-04 01:20:34 +0300

    String Length - Syntax, Functions and Statements - Lab - JS Advanced

---
+++ 03. JS Advanced/01. Syntax, Functions and Statements - Lab/02. String Length/02. String Length.js
@@ -0,0 +1,8 @@
+function stringLength(first, second, third) {
+
+    console.log(first.length + second.length + third.length);
+    console.log(Math.floor((first.length + second.length + third.length) / 3));
+
+}
+stringLength('chocolate', 'ice cream', 'cake');
+stringLength('pasta', '5', '22.3');
