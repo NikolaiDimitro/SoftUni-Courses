11d36b738bac13a4d53427bab8915bbf299416ac
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2025-09-05 21:10:39 +0300

    Piece of Pie = Array and Nested Array - JS Advanced

---
+++ 03. JS Advanced/03. Arrays and Nested Arrays - Lab/07. Piece of Pie/07. Piece of Pie.js
@@ -0,0 +1,10 @@
+function pieceOfPie(arr, firstTaste, secondTaste) {
+
+    let indexOfFirstTaste = arr.indexOf(firstTaste);
+    let indexOfSecondTaste = arr.indexOf(secondTaste);
+    let result = arr.slice(indexOfFirstTaste, indexOfSecondTaste + 1);
+    return result;
+
+}
+pieceOfPie(['Pumpkin Pie', 'Key Lime Pie', 'Cherry Pie', 'Lemon Meringue Pie', 'Sugar Cream Pie'], 'Key Lime Pie', 'Lemon Meringue Pie');
+pieceOfPie(['Apple Crisp', 'Mississippi Mud Pie', 'Pot Pie', 'Steak and Cheese Pie', 'Butter Chicken Pie', 'Smoked Fish Pie'], 'Pot Pie', 'Smoked Fish Pie');
