c3256c3528dd1e5010bdfc1ea8c8e9a7f3b318e4
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2026-07-30 23:14:56 +0300

    Greater Number

---
+++ JS Basic/03. Conditional Statements - Lab/02. Greater Number.js
@@ -0,0 +1,16 @@
+function greaterNumber(numb1, numb2) {
+
+    if (numb1 >= numb2) {
+
+        console.log(numb1);
+
+    } else {
+
+        console.log(numb2);
+
+    }
+}
+greaterNumber(5, 3);
+greaterNumber(3, 5);
+greaterNumber(10, 10);
+greaterNumber(-5, 5);
